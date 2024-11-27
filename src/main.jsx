71e32c166561242a77669568912cdce853ba1b80
MeiyinPU <mei-yin.pu@oclock.school>
2024-11-27 23:16:19 +0100

    update:  package.json

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
