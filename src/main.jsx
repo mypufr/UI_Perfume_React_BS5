12293cb508fa656a0c2d03daa6197c25a682970d
MeiyinPU <mei-yin.pu@oclock.school>
2024-11-29 01:22:29 +0100

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
